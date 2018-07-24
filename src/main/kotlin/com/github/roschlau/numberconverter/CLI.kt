eeafcf9911253fc02a36cc200431da5e5f9ec4f8
Robin Roschlau <robin.roschlau@postadress.de>
2018-07-24 17:45:39 +0200

    :art: Slim IO down

--- src/main/kotlin/com/github/roschlau/numberconverter/CLI.kt
+++ src/main/kotlin/com/github/roschlau/numberconverter/CLI.kt
@@ -4,10 +4,17 @@ package com.github.roschlau.numberconverter
 fun main(args: Array<String>) {
     while (true) {
         print("> ")
-        IO.readLine()
+        readLine()
             .map { it!! }
             .map(::convert)
-            .flatMap(IO.Companion::display)
+            .flatMap(::display)
             .run()
     }
 }
+
+fun display(result: Result<String>): IO<Unit> = result.fold(
+    ifLeft = { displayError(it.msg) },
+    ifRight = { displayResult(it) }
+)
+fun displayResult(result: String): IO<Unit> = println(result)
+fun displayError(msg: String): IO<Unit> = println("ERROR: $msg")
--- src/main/kotlin/com/github/roschlau/numberconverter/IO.kt
+++ src/main/kotlin/com/github/roschlau/numberconverter/IO.kt
@@ -8,15 +8,7 @@ class IO<T>(val run: () -> T) {
     fun <R> map(transform: (T) -> R): IO<R> = IO { transform(run()) }
     fun <R> flatMap(t: (T) -> IO<R>): IO<R> = IO { t(run()).run() }
 
-    companion object {
-
-        fun readLine(): IO<String?> = IO { kotlin.io.readLine() }
-
-        fun display(result: Result<String>): IO<Unit> = result.fold(
-            ifLeft = { displayError(it.msg) },
-            ifRight = { displayResult(it) }
-        )
-        fun displayResult(result: String): IO<Unit> = IO { println(result) }
-        fun displayError(msg: String): IO<Unit> = IO { println("ERROR: $msg") }
-    }
 }
+
+fun readLine(): IO<String?> = IO { kotlin.io.readLine() }
+fun println(msg: String = "") = IO { kotlin.io.println(msg) }
