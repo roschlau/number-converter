3699a8b0b4a747905d59bae0caaa43bdaa83274a
Robin Roschlau <robin.roschlau@gmail.com>
2018-07-31 23:44:37 +0200

    Add "/convert" endpoint

--- web-api/src/main/kotlin/com/github/roschlau/numberconverter/web/api/App.kt
+++ web-api/src/main/kotlin/com/github/roschlau/numberconverter/web/api/App.kt
@@ -1,11 +1,15 @@
 package com.github.roschlau.numberconverter.web.api
 
+import arrow.core.Either
+import com.github.roschlau.numberconverter.convert
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
 import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
@@ -15,7 +19,18 @@ fun Application.main() {
         header(HttpHeaders.Server, "Number Converter Web API")
     }
     install(CallLogging)
+
     install(Routing) {
+        get("/convert/{number}") {
+            val number = call.parameters["number"]
+                ?: return@get call.respond(HttpStatusCode.BadRequest)
+            val result = convert(number)
+            when (result) {
+                is Either.Right -> call.respondText(result.b)
+                is Either.Left -> call.respond(HttpStatusCode.BadRequest, result.a.msg)
+            }
+        }
+
         get("/health") {
             call.respondText("Hello there")
         }
