f4889deadcc9bbb66112a030858b657dc171aeac
Robin Roschlau <robin.roschlau@gmail.com>
2018-08-01 15:59:55 +0200

    Move exit logic from readLoop to repl

--- cli/src/main/kotlin/com/github/roschlau/numberconverter/cli/Repl.kt
+++ cli/src/main/kotlin/com/github/roschlau/numberconverter/cli/Repl.kt
@@ -3,6 +3,14 @@ package com.github.roschlau.numberconverter.cli
 import java.io.BufferedReader
 import java.io.PrintStream
 
+/**
+ * Starts a Read-Eval-Print-Loop.
+ *
+ * It prints a [prompt] to [out], read the next line from [in], call [eval] with that line, print the result to [out]
+ * and then starts over again.
+ *
+ * The loop will exit if the line read from [in] equals one of the [exitKeywords].
+ */
 fun repl(
     prompt: String = "> ",
     exitKeywords: Set<String> = setOf("exit"),
@@ -10,14 +18,20 @@ fun repl(
     out: PrintStream = System.out,
     eval: (String) -> String
 ) {
-    readLoop(prompt, exitKeywords, `in`, out)
+    readLoop(prompt, `in`, out)
+        .takeWhile { it !in exitKeywords }
         .map(eval)
         .forEach(out::println)
 }
 
+/**
+ * Provides the Read-Loop of a REPL.
+ *
+ * It generates a sequence that will print the [prompt] to [out], read the next line from [in], trim it and return it
+ * every time a new value is requested. The sequence will terminate if the read line is null (e.g. when EOF is reached)
+ */
 fun readLoop(
     prompt: String,
-    exitKeywords: Set<String>,
     `in`: BufferedReader,
     out: PrintStream
 ): Sequence<String> =
@@ -25,5 +39,4 @@ fun readLoop(
         out.print(prompt)
         `in`.readLine()
             ?.trim()
-            ?.takeUnless { it in exitKeywords }
     }
