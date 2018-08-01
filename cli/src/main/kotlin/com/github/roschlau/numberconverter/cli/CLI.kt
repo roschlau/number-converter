3791655cfee645fa4936ae73a8a592df28cfd046
Robin Roschlau <robin.roschlau@gmail.com>
2018-08-01 14:53:33 +0200

    Refactor REPL to do the printing itself

--- cli/src/main/kotlin/com/github/roschlau/numberconverter/cli/CLI.kt
+++ cli/src/main/kotlin/com/github/roschlau/numberconverter/cli/CLI.kt
@@ -6,19 +6,26 @@ import com.github.roschlau.numberconverter.convert
 
 
 fun main(args: Array<String>) {
-    repl(handleInput = ::convert andThen ::display)
+    repl(eval = ::convert andThen ::show)
 }
 
-inline fun repl(prompt: String = "> ", handleInput: (String) -> Unit) {
-    while (true) {
+fun repl(
+    prompt: String = "> ",
+    exitKeywords: Set<String> = setOf("exit"),
+    eval: (String) -> String
+) {
+    readLoop(prompt, exitKeywords)
+        .map(eval)
+        .forEach(::println)
+}
+
+fun readLoop(prompt: String, exitKeywords: Set<String>): Sequence<String> =
+    generateSequence {
         print(prompt)
-        readLine()?.let(handleInput)
+        readLine()?.takeUnless { it in exitKeywords }
     }
-}
 
-fun display(result: Result<String>): Unit = result.fold(
-    ifLeft = { displayError(it.msg) },
-    ifRight = { displayResult(it) }
+fun show(result: Result<String>): String = result.fold(
+    ifLeft = { "ERROR: ${it.msg}" },
+    ifRight = { it }
 )
-fun displayResult(result: String): Unit = println(result)
-fun displayError(msg: String): Unit = println("ERROR: $msg")
